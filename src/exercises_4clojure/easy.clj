18e2f41cba02d4e850fd7866f15d510cdbf126f7
Tomás Infante <quete@importa.com>
2019-09-18 11:27:32 -0300

    easy 146

--- src/exercises_4clojure/easy.clj
+++ src/exercises_4clojure/easy.clj
@@ -1270,6 +1270,35 @@
   (println (str "problem 143: " (v143 s143-b))))
 
 
+;; problem 146
+(defn v146
+  [x]
+  (every? identity
+    [
+     (= (x '{a {p 1, q 2}
+             b {m 3, n 4}})
+        '{[a p] 1, [a q] 2
+          [b m] 3, [b n] 4})
+     (= (x '{[1] {a b c d}
+             [2] {q r s t u v w x}})
+        '{[[1] a] b, [[1] c] d,
+          [[2] q] r, [[2] s] t,
+          [[2] u] v, [[2] w] x})
+     (= (x '{m {1 [a b c] 3 nil}})
+        '{[m 1] [a b c], [m 3] nil})
+     ]))
+
+(def s146
+  (fn [m] (into {} (for [[k v] m
+                         [vk vv] v]
+                     [[k vk] vv])))
+  )
+
+(defn p146
+  []
+  (println (str "problem 146: " (v146 s146))))
+
+
 ;; problem 147
 (defn v147
   [x]
